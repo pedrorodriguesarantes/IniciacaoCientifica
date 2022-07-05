19e7ca2526e8d3a9b741359fa912bd8b4aa10aab
Pedro Rodrigues Arantes de Oliveira <pedrorodriguesarantes@gmail.com>
2022-07-05 01:38:27 -0400

    [MINING] Dados Comentarios AngularJS

--- Funcoes/arquivo.py
+++ Funcoes/arquivo.py
@@ -1,12 +1,23 @@
 import pandas as pd
 import warnings
 
-def unir_arquivo(caminhos):
+def unir_arquivo_excel(caminhos):
     final = pd.DataFrame()
 
     for caminho in caminhos:
         warnings.filterwarnings('ignore')
         df = pd.read_excel(caminho, index_col = 0)
+        
+        final = final.append(df)
+    
+    return final
+
+def unir_arquivo_csv(caminhos):
+    final = pd.DataFrame()
+
+    for caminho in caminhos:
+        warnings.filterwarnings('ignore')
+        df = pd.read_csv(caminho, sep = ";", index_col=None)
         
         final = final.append(df)
     
